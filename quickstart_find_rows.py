cdeedfd9ae316606734f2ac3440cf200331a188e
skedwards88 <skedwards88@gmail.com>
2024-12-19 08:21:43 -0800

    updates to find

--- quickstart_find_rows.py
+++ quickstart_find_rows.py
@@ -7,16 +7,15 @@ def main():
 
     table = database.get_table("quickstart_table2") # todo revert 2
 
-    print(table.list_indexes())
     # Find documents that match a filter
-    print("\nFinding books with rating greater than 4.7...")
+    # print("\nFinding books with rating greater than 4.7...")
 
-    rating_cursor = table.find({"rating": {"$gt": 4.7}}) # todo change duration_cursor var name in current quickstart
+    # rating_cursor = table.find({"rating": {"$gt": 4.7}}) # todo change duration_cursor var name in current quickstart
 
-    for row in rating_cursor:
-        print(f"{row['title']} is rated {row['rating']}")
+    # for row in rating_cursor:
+    #     print(f"{row['title']} is rated {row['rating']}")
 
-    # Perform a vector search to find the closest match to a search string
+    # # Perform a vector search to find the closest match to a search string
     print("\nUsing vector search to find a single scary novel...")
 
     single_vector_match = table.find_one(
@@ -28,17 +27,17 @@ def main():
     # Combine a filter, vector search, and projection to find the 3 books with
     # more than 400 pages that are the closest matches to a search string,
     # and just return the title and author
-    print("\nUsing filters and vector search to find 3 books with more than 400 pages that are set in the arctic, returning just the title and author...")
+    # print("\nUsing filters and vector search to find 3 books with more than 400 pages that are set in the arctic, returning just the title and author...")
 
-    vector_cursor = table.find(
-        {"numberOfPages": {"$gt": 400}},
-        # sort={"summaryGenresVector": "A book set in the arctic"},
-        limit=3,
-        projection={"title": True, "author": True}
-    )
+    # vector_cursor = table.find(
+    #     {"numberOfPages": {"$gt": 400}},
+    #     # sort={"summaryGenresVector": "A book set in the arctic"},
+    #     limit=3,
+    #     projection={"title": True, "author": True}
+    # )
 
-    for row in vector_cursor:
-        print(row)
+    # for row in vector_cursor:
+    #     print(row)
 
 
 if __name__ == "__main__":
--- quickstart_find_rows_snake.py
+++ quickstart_find_rows_snake.py
@@ -5,13 +5,11 @@ from astrapy.constants import VectorMetric
 
 def main():
     database = connect_to_database()
-    print(database.list_tables())
 
     table = database.get_table("quickstart_table_snake") # todo revert 2
 
-    print(table.list_indexes())
     # Find documents that match a filter
-    print("\nFinding books with rating greater than 4.7...")
+    print("\n@@@@@ Finding books with rating greater than 4.7...")
 
     rating_cursor = table.find({"rating": {"$gt": 4.7}}) # todo change duration_cursor var name in current quickstart
 
@@ -19,28 +17,28 @@ def main():
         print(f"{row['title']} is rated {row['rating']}")
 
     # Perform a vector search to find the closest match to a search string
-    # print("\nUsing vector search to find a single scary novel...")
+    print("\n@@@@@ Using vector search to find a single scary novel...")
 
-    # single_vector_match = table.find_one(
-    #     {}, sort={"summaryGenresVector": "A scary novel"}
-    # )
+    single_vector_match = table.find_one(
+        {}, sort={"summary_genres_vector": "A scary novel"}
+    )
 
-    # print(f"{single_vector_match['title']} is a scary novel")
+    print(f"{single_vector_match['title']} is a scary novel")
 
-    # # Combine a filter, vector search, and projection to find the 3 books with
-    # # more than 400 pages that are the closest matches to a search string,
-    # # and just return the title and author
-    # print("\nUsing filters and vector search to find 3 books with more than 400 pages that are set in the arctic, returning just the title and author...")
+    # Combine a filter, vector search, and projection to find the 3 books with
+    # more than 400 pages that are the closest matches to a search string,
+    # and just return the title and author
+    print("\n@@@@@ Using filters and vector search to find 3 books with more than 400 pages that are set in the arctic, returning just the title and author...")
 
-    # vector_cursor = table.find(
-    #     {"numberOfPages": {"$gt": 400}},
-    #     # sort={"summaryGenresVector": "A book set in the arctic"},
-    #     limit=3,
-    #     projection={"title": True, "author": True}
-    # )
+    vector_cursor = table.find(
+        {"number_of_pages": {"$gt": 400}},
+        sort={"summary_genres_vector": "A book set in the arctic"},
+        limit=3,
+        projection={"title": True, "author": True}
+    )
 
-    # for row in vector_cursor:
-    #     print(row)
+    for row in vector_cursor:
+        print(row)
 
 
 if __name__ == "__main__":
